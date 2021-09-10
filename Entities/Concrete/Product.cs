ef829a1f597f18033ca404fdbf0f8c3993111638
Işıl Özkaynak <59703446+isilozkaynak@users.noreply.github.com>
2021-09-10 19:16:21 +0300

    edited

--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -9,9 +9,11 @@ namespace Entities.Concrete
     {
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
         public string ProductName { get; set; }
         public decimal UnitPrice { get; set; }
         public int GameId { get; set; }
+        public string GameName { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string DescriptionProduct { get; set; }
     }
